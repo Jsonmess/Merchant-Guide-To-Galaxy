e3922c39832ea675a74759e3bdb94152ceee9c96
jsonmess <jsonmess@gmail.com>
2015-11-11 22:00:44 +0800

    Initial Commit

---
+++ Galaxy'sMerchantsGuide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Galaxy'sMerchantsGuide
+//
+//  Created by jsonmess on 15/11/11.
+//  Copyright © 2015年 jsonmess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Galaxy'sMerchantsGuide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Galaxy'sMerchantsGuide
+//
+//  Created by jsonmess on 15/11/11.
+//  Copyright © 2015年 jsonmess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
